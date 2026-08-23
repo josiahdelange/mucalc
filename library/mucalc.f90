01049528fc896178bdd4a7928a729eb77b13fb07
Josiah DeLange <josiah.delange@gmail.com>
2026-08-23 16:02:49 -0400

    Add skeleton structure (likely may change)

---
+++ library/mucalc.f90
@@ -0,0 +1,32 @@
+module mucalc
+    implicit none
+
+contains
+    subroutine slicot_ab13md(fact, n, z, ldz, m, nblock, itype, x, bound, d, g, &
+        &   iwork, dwork, ldwork, zwork, lzwork, info)
+        use, intrinsic :: iso_c_binding
+
+        character(len=*), intent(in) :: fact
+        integer(c_int), intent(in) :: n
+        complex(c_double_complex), intent(in) :: z(ldz, *)
+        integer(c_int), intent(in) :: ldz
+        integer(c_int), intent(in) :: m
+        integer(c_int), intent(in) :: nblock(*)
+        integer(c_int), intent(in) :: itype(*)
+        real(c_double), intent(inout) :: x(*)
+        real(c_double), intent(out) :: bound
+        real(c_double), intent(out) :: d(*)
+        real(c_double), intent(out) :: g(*)
+        integer(c_int), intent(inout) :: iwork(*)
+        real(c_double), intent(inout) :: dwork(*)
+        integer(c_int), intent(in) :: ldwork
+        complex(c_double_complex), intent(inout) :: zwork(*)
+        integer(c_int), intent(in) :: lzwork
+        integer(c_int), intent(out) :: info
+
+        ! Call SLICOT subroutine from Fortran-90 module
+        !
+        !TODO
+        !
+    end subroutine slicot_ab13md
+end module mucalc
